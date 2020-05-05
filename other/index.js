d06b3077aa77a75162c515c06c82b06e00608b48
emosaar <64479668+emosaar@users.noreply.github.com>
2020-05-05 23:50:19 +0800

    20200505

--- other/index.js
+++ other/index.js
@@ -1,4 +1,29 @@
 (function () {
 
+    let data = {
+        menu: [
+            {
+                h1: '食物選單',
+                text: 'Jquery'
+            },
+            {
+                h1: '部落格首頁',
+                text: '版面'
+            },
+            {
+                h1: '圖片選單',
+                text: 'clip-path & picsum'
+            },
+            {
+                h1: '稀飯版',
+                text: 'Bootstrap'
+            },
+        ]
+    }
+
+    let vm = new Vue({
+        el: 'main',
+        data: data
+    })
 
 })();
